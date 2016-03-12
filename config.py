4ba017dfc4fd772bd4c56f1f8b470759da2b0a98
Jesus Roldan <xebuz18@gmail.com>
2016-03-11 21:32:12 -0300

    Move Librarie to Bower

--- config.py
+++ config.py
@@ -6,12 +6,15 @@ class Config(object):
     DEBUG = True
     TESTING = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SEND_FILE_MAX_AGE_DEFAULT = 1
     # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class Development(Config):
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SEND_FILE_MAX_AGE_DEFAULT = 1
     SQLALCHEMY_DATABASE_URI = 'postgres://cuxwxedgorsvlz:JOUSDhsl0fNgMfwyiLu0HQEwMe@ec2-107-21-101-67.compute-1.amazonaws.com:5432/d1mrcoa2cq7kcv'
     # SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
